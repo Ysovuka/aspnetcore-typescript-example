8895ca859609860855e6cab1a76fc71ea87ba317
Joshua Thompson <ysovuka@gmail.com>
2017-07-30 21:43:32 -0500

    Finished Article

---
+++ src/Web/wwwroot/scripts/app.js
@@ -0,0 +1,9 @@
+// Person is now string
+function Welcome(person) {
+    return "Hello, " + person;
+}
+function ClickMeButton() {
+    var user = "Joshua Thompson";
+    document.getElementById("divMsg").innerHTML = Welcome(user);
+}
+//# sourceMappingURL=app.js.map
